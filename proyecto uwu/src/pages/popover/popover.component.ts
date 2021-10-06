1faaf2bbc0b26ebf107b4326c4599aa9efe56c64
Anton Jeldres <antonbransonjeldres@gmail.com>
2021-10-06 16:15:35 -0300

    owo

--- proyecto uwu/src/pages/popover/popover.component.ts
+++ proyecto uwu/src/pages/popover/popover.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ModalController, PopoverController } from "@ionic/angular";
+import { Router } from "@angular/router";
+import { ModalController, PopoverController, AlertController } from "@ionic/angular";
+import { ClosemodComponent } from "../modals/closemod/closemod.component";
 import { PerfilmodComponent } from "../modals/perfilmod/perfilmod.component";
 
 @Component({
@@ -10,7 +12,9 @@ import { PerfilmodComponent } from "../modals/perfilmod/perfilmod.component";
 export class PopoverComponent {
     
     constructor(public popoverController: PopoverController,
-                private modalCtrl: ModalController){}
+                private modalCtrl: ModalController,
+                public alertController: AlertController,
+                private route:Router){}
     
     async perfilModal(){
         const mPerfil = await this.modalCtrl.create({
@@ -20,7 +24,48 @@ export class PopoverComponent {
         await mPerfil.present();
     }
 
+    async ayudaModal(){
+        const mPerfil = await this.modalCtrl.create({
+            component: ClosemodComponent,
+            cssClass: 'small-modal'
+        });
+        await mPerfil.present();
+    }
+
     close(){
         this.popoverController.dismiss();
     }
+
+    async alertClose() {
+        const alert = await this.alertController.create({
+          cssClass: 'my-custom-class',
+          header: 'Confirmación',
+          message: '¿Está seguro que desea salir?',
+          buttons: [
+            {
+              text: 'Cancelar',
+              role: 'cancel',
+              cssClass: 'secondary',
+              handler: (blah) => {
+                console.log('Confirm Cancel: blah');
+              }
+            }, {
+              text: 'Aceptar',
+              handler: () => {
+                this.route.navigate([''])
+                this.close()
+                console.log('Confirm Okay');
+              }
+            }
+          ]
+        });
+    
+        await alert.present();
+      }
+    
+    logout(){
+        
+        
+    }
+
 }
