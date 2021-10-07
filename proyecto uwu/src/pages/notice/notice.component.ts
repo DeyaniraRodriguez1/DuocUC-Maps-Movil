f99b300d9b365c729bef70154e7ada68daa51c77
Anton Jeldres <antonbransonjeldres@gmail.com>
2021-10-07 01:26:14 -0300

    casi final

--- proyecto uwu/src/pages/notice/notice.component.ts
+++ proyecto uwu/src/pages/notice/notice.component.ts
@@ -9,7 +9,7 @@ import { PopoverComponent } from "../popover/popover.component";
 })
 export class NoticeComponent {
 
-    aviso = [{titulo:'No hay clases', des:'alumnos el dia lunes 11 de octubre no hay clases'},{ titulo: 'Recordatorio Prueba', des: 'Recuerden que este jueves hay prueba de ionic'}];
+    aviso = [{id:1,titulo:'No hay clases', des:'Estimados, alumnos el dia lunes 11 de octubre no hay clases'},{ id:2, titulo: 'Recordatorio Prueba', des: 'Recuerden que este jueves hay prueba de ionic'}];
 
     constructor(public popoverController: PopoverController){}
 
@@ -24,7 +24,8 @@ export class NoticeComponent {
     doRefresh(event){
 
         setTimeout(()=>{
-        event.complete();
+        this.aviso.push({id:3, titulo:'Push',des:'refrsheado'},{id:4,titulo:'Otro anuncio',des:'testeo de anuncio descriptivo'},{id:5,titulo:'Renovar TNE',des:'No olviden de renovar su tne para que no tengan que pagar 700 pesos de pasaje uwu'})
+        event.target.complete();
         },2000);
     }
     
